7b2804d42ddd120b8453a98e8b33a9d9ce91a73c
Phuong <kkimphuong22@gmail.com>
2022-04-30 22:20:19 +0700

    add files

---
+++ Dish-Decision-Project/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dish_Decision_Project.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public MainViewModel()
+           {
+            
+            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+        }
+    }
+}
